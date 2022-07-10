86682357b34b5f3adb7e66d34010e5e4f0954ac2
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-07-10 18:20:08 -0300

    fix: deleting some comments

--- src/controllers/transactionsController.ts
+++ src/controllers/transactionsController.ts
@@ -54,89 +54,27 @@ export async function payments(req: Request, res: Response){
     //Somente cartões cadastrados devem poder comprar
     const cardId: number = +req.body.cardId;
     const cardInfo = await cardServices.findCardById(cardId)
- /*    const cardInfo = await cardRepository.findById(cardId);
-    if(cardInfo == undefined){
-        throw {
-            type: "CARD DOESN'T EXIST"
-        }
-    } */
     //Somente cartões ativos devem poder comprar 
     verifyCardRegistration.verifyCardActivation(cardInfo)
-/*     if(cardInfo.password == null){
-        throw {
-            type: "CARD IS NOT ACTIVATED"
-        }
-    } */
-    //Somente cartões não expirados devem poder comprar
-/*     const formatExpiredData = dayjs(`01/${cardInfo.expirationDate}`).format();
-    const cardIsExpired = dayjs().isAfter(formatExpiredData)
-    if(cardIsExpired){
-        throw{
-            type: "CARD IS ALREADY EXPIRED"
-        }
-    } */
     verifyCardExpiration.verifyExpiration(cardInfo)
 
     //Somente cartões não bloqueados devem poder comprar
     verifyBlock.verifyifIsBlock(cardInfo)
 
-/*     if(cardInfo.isBlocked){
-        throw {
-            type: "CARD IS ALREADY BLOCKED"
-        }
-    } */
-
     //A senha do cartão deverá ser recebida e verificada para garantir a segurança da requisição
     verifyPassword.verifyCorrectPassword(password, cardInfo)
-/*     const isPasswordCorrect = bcrypt.compareSync(password, cardInfo.password);
-    if(!isPasswordCorrect){
-        throw{
-            type: "INCORRECT PASSWORD"
-        }
-    } */
     
     //Somente estabelecimentos cadastrados devem poder transacionar
     const businessesRegistered = await transactionsServices.findIfCompanyIsRegistered(businessId)
-/*     const businessesRegistered = await businessRepository.findById(businessId);
-    if(!businessesRegistered){
-        throw{
-            type: "BUSINESS NOT REGISTERED"
-        }
-    } */
     //Somente estabelecimentos do mesmo tipo do cartão devem poder transacionar com ele
     verifyIfTypeMatchs.verifyMatchType(cardInfo, businessesRegistered)
-
-/*     if(cardInfo.type !== businessesRegistered.type){
-        throw{
-            type: "PAYMENT FAILLED: INCORRECT TYPE"
-        }
-    } */
     
     //O cartão deve possuir saldo suficiente para cobrir o montante da compra
     const recharges = await cardServices.rechargeCardById(cardId)
 
-/*     const recharges = await rechargeRepository.findByCardId(cardId);
-    let rechargeAmount: number = 0;
-        recharges?.map(recharge => {
-            rechargeAmount += recharge.amount
-        }) */
-
     const payments = await cardServices.paymentCardById(cardId)
 
-/*     const payments = await paymentRepository.findByCardId(cardId);
-    let paymentAmount: number = 0;
-        payments?.map(payment => {
-            paymentAmount += payment.amount
-        }) */
-
-        verifyFundsForPayment.verifyFunds(recharges.rechargeAmount, payments.paymentAmount, amount)
-
-/*     let NewbalanceCard = recharges.rechargeAmount - payments.paymentAmount - amount;
-    if(NewbalanceCard < 0){
-        throw{
-            type: "INSUFFICIENT FUNDS"
-        }
-    } */
+    verifyFundsForPayment.verifyFunds(recharges.rechargeAmount, payments.paymentAmount, amount)
 
     //A compra deve ser persistida
 
