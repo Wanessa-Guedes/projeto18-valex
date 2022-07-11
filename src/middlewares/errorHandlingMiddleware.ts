a5859715d60b2d33e863dee6fe4ae5abf6b96592
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-07-11 10:34:53 -0300

    fix: deploy

--- src/middlewares/errorHandlingMiddleware.ts
+++ src/middlewares/errorHandlingMiddleware.ts
@@ -1,8 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export default async function handleError(error, req: Request, res: Response, next: NextFunction) {
-    if(error.type == "NOT FOUND" || error.type == "CARD DOESN'T EXIST" || 
-        error.type == "EMPLOYEE NOT REGISTERED IN THIS COMPANY OR CARD DOESN'T BELONG TO EMPLOYEE"){
+    if(error.type == "NOT FOUND" || error.type == "CARD DOESN'T EXIST"){
         return res.status(404).send(`${error.type}`);
     }
 
@@ -12,12 +11,13 @@ export default async function handleError(error, req: Request, res: Response, ne
         error.type == "CARD IS BLOCKED" || error.type == "INCORRECT PASSWORD" ||
         error.type == "CARD IS NOT BLOCKED" || error.type == "CARD IS NOT ACTIVATED" || 
         error.type == "BUSINESS NOT REGISTERED" || error.type == "PAYMENT FAILLED: INCORRECT TYPE"
-        || error.type == "INSUFFICIENT FUNDS" ){
+        || error.type == "INSUFFICIENT FUNDS" || 
+        error.type == "EMPLOYEE NOT REGISTERED IN THIS COMPANY OR CARD DOESN'T BELONG TO EMPLOYEE"){
         return res.status(401).send(`${error.type}`);
     }
 
     if(error.details){
-        return res.status(402).send(`${error.message}`);
+        return res.status(422).send(`${error.message}`);
     }
 
     return res.status(500).send("Internal Server Error")
