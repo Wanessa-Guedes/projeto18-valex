397defa692acb81b90c0f179a39f0ff6c7bd54fa
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-07-11 09:59:21 -0300

    fix: removing card visualization from main and creating a branch with this router

--- src/routes/cardRouter.ts
+++ src/routes/cardRouter.ts
@@ -2,7 +2,7 @@ import {Router} from "express";
 
 import { schemaValidator } from "./../middlewares/schemaValidator.js";
 import { typeCardSchema } from "../schemas/typeCardSchema.js";
-import { cardBlock, cardTransactions, cardUnblock, createCard, getCards } from "../controllers/cardController.js";
+import { cardBlock, cardTransactions, cardUnblock, createCard } from "../controllers/cardController.js";
 import { activateCard } from "../controllers/cardController.js";
 import { activationCardSchema } from "../schemas/cardActivationSchema.js";
 import { passwordSchema } from "../schemas/verifyPasswordSchema.js";
@@ -11,7 +11,6 @@ const cardRouter = Router();
 
 cardRouter.post("/card", schemaValidator(typeCardSchema), createCard)
 cardRouter.post("/activatecard", schemaValidator(activationCardSchema), activateCard)
-cardRouter.get("/card", getCards)
 cardRouter.get("/cardbalance/:cardId", cardTransactions)
 cardRouter.put("/card/block/:cardId", schemaValidator(passwordSchema), cardBlock)
 cardRouter.put("/card/unblock/:cardId", schemaValidator(passwordSchema), cardUnblock)
