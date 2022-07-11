3678dd4b6cce75019cba1ec9a1f8c3a8c1bf8d12
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-07-11 11:14:30 -0300

    fix: removing comments

--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -13,26 +13,19 @@ import { verifyPassword } from "../utils/verifyPassword.js";
 import { verifyBlock } from "../utils/verifyBlock.js";
 
 export async function createCard(req: Request, res: Response) {
-        //
         const {employeeId, cardType}:{employeeId: number, cardType: cardRepository.TransactionTypes} = req.body;
-
-        // A chave de API deverá ser recebida no header x-api-key
+        
         const apiKey = req.headers['x-api-key'] as string;
         verifyApiKey.verifyKey(apiKey)
 
-        // A chave de API deve ser possuida por alguma empresa
         await cardServices.validateAPIKey(apiKey)
 
-        // Somente empregados cadastrados devem possuir cartões
         const validEmployee = await cardServices.registeredEmployee(employeeId)
 
-        // Empregados não podem possuir mais de um cartão do mesmo tipo
         await cardServices.confirmCardType(employeeId, cardType)
         
-        //GERANDO INFORMAÇÕES DO CARTÃO
         const infosCard = generateInfosCard.generateCard(validEmployee);
 
-        // criação do cartão
         const cardData = {
         employeeId: employeeId,
         number: infosCard.creditCarNumber,
@@ -55,19 +48,15 @@ export async function activateCard(req: Request, res: Response) {
     const cardId: number = +req.body.cardId;
     const securityCode: string = req.body.securityCode;
     const password: string = req.body.password;
-    // Somente cartões cadastrados devem ser ativados 
+
     const cardInfo = await cardServices.findCardById(cardId);
 
-    // Somente cartões não expirados devem ser ativados
     verifyCardExpiration.verifyExpiration(cardInfo)
 
-    //Cartões já ativados (com senha cadastrada) não devem poder ser ativados de novo
     verifyCardRegistration.verifyRegister(cardInfo)
 
-    // O CVC deverá ser recebido e verificado para garantir a segurança da requisição
     verifyCVC.cardCVC(securityCode, cardInfo)
 
-    // A senha do cartão deverá ser persistida de forma criptografada por ser um dado sensível
     const hash = 10;
     const passwordHasehd = bcrypt.hashSync(password, hash);
 
@@ -78,17 +67,12 @@ export async function activateCard(req: Request, res: Response) {
 }
 
 export async function cardTransactions(req: Request, res: Response) {
-    // Nessa rota, empregados podem visualizar o saldo de um cartão e as transações do mesmo. 
-    // Para isso, precisamos do identificador do cartão.
 
-    // Somente cartões cadastrados devem poder ser visualizados
     const cardId: number = +req.params.cardId;
     const cardInfo = await cardServices.findCardById(cardId)
 
-    // Somente cartões ativos tem transações
     verifyCardRegistration.verifyCardActivation(cardInfo)
 
-    // O saldo de um cartão equivale a soma de suas recargas menos a soma de suas compras
     const rechargeInfo = await cardServices.rechargeCardById(cardId)
 
     const paymentInfo = await cardServices.paymentCardById(cardId);
@@ -100,24 +84,17 @@ export async function cardTransactions(req: Request, res: Response) {
 }
 
 export async function cardBlock(req: Request, res: Response) {
-    //Nessa rota, empregados podem bloquear cartões. 
-    //Para um cartão ser bloqueado precisamos do identificador(CVC ou id?) e da senha do mesmo.
 
-    // Somente cartões cadastrados devem ser bloqueados
     const cardId: number = +req.params.cardId;
     const password: string = req.body.password;
     const cardInfo = await cardServices.findCardById(cardId)
 
-    // Somente cartões ativos podem ser bloqueados
     verifyCardRegistration.verifyCardActivation(cardInfo)
 
-    //A senha do cartão deverá ser recebida e verificada para garantir a segurança da requisição
     verifyPassword.verifyCorrectPassword(password, cardInfo)
-    
-    //Somente cartões não expirados devem ser bloqueados
+
     verifyCardExpiration.verifyExpiration(cardInfo)
 
-    //Somente cartões não bloqueados devem ser bloqueados
     verifyBlock.verifyifIsBlock(cardInfo)
 
     await cardRepository.update(cardId, {isBlocked: true});
@@ -126,25 +103,18 @@ export async function cardBlock(req: Request, res: Response) {
 }
 
 export async function cardUnblock(req: Request, res: Response) {
-    //Nessa rota, empregados podem desbloquear cartões. 
-    //Para um cartão ser desbloqueado precisamos do identificador e da senha do mesmo.
 
-    //Somente cartões cadastrados devem ser desbloqueados
     const cardId: number = +req.params.cardId;
     const password: string = req.body.password;
 
     const cardInfo = await cardServices.findCardById(cardId)
-    
-    // Somente cartões ativos podem ser desbloqueados
+
     verifyCardRegistration.verifyCardActivation(cardInfo)
-    
-    //A senha do cartão deverá ser recebida e verificada para garantir a segurança da requisição
+
     verifyPassword.verifyCorrectPassword(password, cardInfo)
 
-    //Somente cartões não expirados devem ser desbloqueados
     verifyCardExpiration.verifyExpiration(cardInfo)
 
-    //Somente cartões bloqueados devem ser desbloqueados
     verifyBlock.verifyIfIsUnblock(cardInfo)
 
     await cardRepository.update(cardId, {isBlocked: false});
--- src/controllers/transactionsController.ts
+++ src/controllers/transactionsController.ts
@@ -14,8 +14,7 @@ import { verifyIfTypeMatchs } from "../utils/verifyIfTypeMatchs.js";
 import { verifyFundsForPayment } from "../utils/verifyFundsForPayment.js";
 
 export async function recharges(req: Request, res: Response) {
-    //Nessa rota, empresas com uma chave de API válida podem recarregar cartões de seus empregados.
-    // Para um cartão ser recarregado precisamos do identificador do mesmo.
+
     const amount: number = +req.body.amount;
     const cardId: number = +req.body.cardId;
     const employeeId: number = +req.body.employeeId;
@@ -23,56 +22,44 @@ export async function recharges(req: Request, res: Response) {
     const apiKey = req.headers['x-api-key'] as string;
     verifyApiKey.verifyKey(apiKey)
 
-    //Somente cartões cadastrados devem receber recargas
     const cardInfo = await cardServices.findCardById(cardId)
 
-    //Somente cartões ativos devem receber recargas
     verifyCardRegistration.verifyCardActivation(cardInfo)
 
-    //Somente cartões não expirados devem receber recargas
     verifyCardExpiration.verifyExpiration(cardInfo)
 
-    //Somente empregados da empresa podem ter o cartão recarregado
     await transactionsServices.findIfEmployeeFromCompanyAndCardFromEmployee(apiKey, employeeId, cardId)
-    
-    //A recarga deve ser persistida
+
     await rechargeRepository.insert({cardId, amount});
 
     res.sendStatus(200);
 }
 
 export async function payments(req: Request, res: Response){
-    // TODO: Somente montantes maiores que 0 deveram ser aceitos
+
     const password: string = req.body.password;
     const businessId: number = +req.body.businessId;
     const amount: number = +req.body.amount;
 
-    //Somente cartões cadastrados devem poder comprar
     const cardId: number = +req.body.cardId;
     const cardInfo = await cardServices.findCardById(cardId)
-    //Somente cartões ativos devem poder comprar 
+
     verifyCardRegistration.verifyCardActivation(cardInfo)
     verifyCardExpiration.verifyExpiration(cardInfo)
 
-    //Somente cartões não bloqueados devem poder comprar
     verifyBlock.verifyifIsBlock(cardInfo)
 
-    //A senha do cartão deverá ser recebida e verificada para garantir a segurança da requisição
     verifyPassword.verifyCorrectPassword(password, cardInfo)
-    
-    //Somente estabelecimentos cadastrados devem poder transacionar
+
     const businessesRegistered = await transactionsServices.findIfCompanyIsRegistered(businessId)
-    //Somente estabelecimentos do mesmo tipo do cartão devem poder transacionar com ele
+
     verifyIfTypeMatchs.verifyMatchType(cardInfo, businessesRegistered)
-    
-    //O cartão deve possuir saldo suficiente para cobrir o montante da compra
+
     const recharges = await cardServices.rechargeCardById(cardId)
 
     const payments = await cardServices.paymentCardById(cardId)
 
     verifyFundsForPayment.verifyFunds(recharges.rechargeAmount, payments.paymentAmount, amount)
-
-    //A compra deve ser persistida
 
     await paymentRepository.insert({ cardId, businessId, amount });
     res.sendStatus(200);
